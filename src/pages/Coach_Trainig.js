8e3e478758f9a69d54664d7f908ef62019282724
Khusrav <khusravjon7202@gmail.com>
2023-05-16 03:16:32 +0300

    страница услуг и некоторые праки по второй итерации

--- src/pages/Coach_Trainig.js
+++ src/pages/Coach_Trainig.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {Card} from "../components/Card/Card";
 
 export const Coach_Trainig = () => {
     return(
-        <h2>Coach_Trainig page</h2>
+        <div className={"wrapper_all_blog"}>
+            <div className="faq_header">
+                <p>Blog</p>
+            </div>
+            <div className={"wrapper_catalog_items_blog container"}>
+                <div className="catalog_item_blog" id={"active-red-catalog-item-blog"}>Be Better</div>
+                <div className="catalog_item_blog">Love Better</div>
+                <div className="catalog_item_blog">Do Better</div>
+                <div className="catalog_item_blog">Coach Better</div>
+
+            </div>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                    <div className={"col-sm-4 col"}><Card/></div>
+                </div>
+            </div>
+        </div>
     );
 }
